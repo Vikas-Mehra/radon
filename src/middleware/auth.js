b37ac1c2c1e3cbc043ae93d0b0d8fc997c2c7e54
Vikas-Mehra <106535864+Vikas-Mehra@users.noreply.github.com>
2022-06-15 20:51:46 +0530

    Create POSTS on User's Wall

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,12 +1,44 @@
-const authenticate = function(req, req, next) {
-    //check the token in request header
-    //validate this token
+const jwt = require("jsonwebtoken");
 
-    next()
-}
+const authenticate = function (req, res, next) {
+  //check the token in request header
+  //validate this token
+  let token = req.headers["x-auth-token"];
+  if (!token) {
+    return res.send({ status: false, msg: "ERROR: Token must be present" });
+  }
+  next();
+};
 
+const authorise = function (req, res, next) {
+  // comapre the logged in user's id and the id in request
+  let token = req.headers["x-auth-token"];
+  let decodedToken = jwt.verify(token, "functionup-radon");
 
-const authorise = function(req, res, next) {
-    // comapre the logged in user's id and the id in request
-    next()
-}
+  if (decodedToken.userId !== req.params.userId) {
+    return res.send({
+      msg: "ERROR: User Logged is NOT allowed to modify the requested User's Data.",
+    });
+  }
+  next();
+};
+
+module.exports.authenticate = authenticate;
+module.exports.authorise = authorise;
+
+// const jwt = require("jsonwebtoken");
+// let middlewareAuth = function (req, res, next) {
+//   let token = req.headers["x-auth-token"];
+//   if (!token) {
+//     return res.send({ status: false, msg: "ERROR: Token must be present" });
+//   }
+
+//   let decodedToken = jwt.verify(token, "functionup-radon");
+
+//   if (decodedToken.userId !== req.params.userId) {
+//     return res.send({ msg: "ERROR: User Logged is NOT allowed to modify the requested User's Data." });
+//   }
+//   next();
+// };
+
+// module.exports.midAuth = middlewareAuth;
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,21 +1,39 @@
-const mongoose = require('mongoose');
-
-const userSchema = new mongoose.Schema( {
+const mongoose = require("mongoose");
+const userSchema = new mongoose.Schema(
+  {
     firstName: String,
     lastName: String,
     mobile: {
-        type: String,
-
-        required: true
+      type: String,
+      required: true,
     },
     emailId: String,
     password: String,
     gender: {
-        type: String,
-        enum: ["male", "female", "other"]
+      type: String,
+      enum: ["male", "female", "other"],
+    },
+    isDeleted: {
+      type: Boolean,
+      default: false,
     },
     age: Number,
-    posts: {type: [], deafult: []}
-}, { timestamps: true });
+    posts: {
+      type: [],
+      default: [],
+    },
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('User', userSchema)
+module.exports = mongoose.model("newUser", userSchema);
+
+// {
+//     "firstName": "Akash",
+//     "lastName": "Moon",
+//     "mobile": "9284029768",
+//     "emailId": "moon@gmail.com",
+//     "password": "abc123",
+//     "gender": "male",
+//     "age": "21"
+// }
